cd787ef28ef5bd09975362772e3dba517df3d1db
Amol Sontakke <amols@proximabiz.com>
2024-04-24 12:10:24 +0530

    Added stats with effects
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/modules/dashboard/components/page-statistics/page-statistics.component.ts
+++ src/app/modules/dashboard/components/page-statistics/page-statistics.component.ts
@@ -7,12 +7,13 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-page-statistics',
   standalone: true,
-  imports: [CommonModule, TooltipModule,SkeletonModule],
+  imports: [CommonModule, TooltipModule, SkeletonModule],
   templateUrl: './page-statistics.component.html',
-  styleUrl: './page-statistics.component.scss'
+  styleUrl: './page-statistics.component.scss',
 })
 export class PageStatisticsComponent {
   @Input() public statistics: Statistics[] = [];
   @Input() public loading: boolean = true;
 
+  public classes = [];
 }
