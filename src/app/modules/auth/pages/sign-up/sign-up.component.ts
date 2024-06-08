1fb7ccb2280c2d426bf2f2b18857375602089645
Amol Sontakke <amols@proximabiz.com>
2024-06-08 19:38:20 +0530

    Added sign up
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -6,6 +6,8 @@ import { ButtonComponent } from 'src/app/shared/components/button/button.compone
 import { LanguageService } from 'src/app/core/services/language.service';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { CommonModule } from '@angular/common';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-sign-up',
@@ -32,6 +34,7 @@ export class SignUpComponent implements OnInit {
   private languageService: LanguageService = inject(LanguageService);
   private router: Router = inject(Router);
   private formBuilder: FormBuilder = inject(FormBuilder);
+  private messageService: MessageService = inject(MessageService);
 
   constructor() {
     this.language = this.languageService.languageConstants;
@@ -41,18 +44,45 @@ export class SignUpComponent implements OnInit {
     this.passwordTextType = !this.passwordTextType;
   }
 
+  public onClickTerm(): void {
+    this.form.controls['acceptTerm'].setValue(!this.form.controls['acceptTerm'].value);
+  }
+
   public ngOnInit(): void {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+      acceptTerm: [false],
     });
   }
 
   public onSubmit(): void {
     this.submitted = true;
-    if (this.form.valid) {
-      this.router.navigate(['/admin/sign-in']);
+    if (this.validateForm) {
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.form.controls['email'].value, this.form.controls['password'].value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          this.messageService.add({
+            severity: 'success',
+            summary: 'User Created',
+            detail: 'User sign up successfully',
+          });
+          this.router.navigate(['/admin/sign-in']);
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          this.messageService.add({ severity: 'error', summary: 'Failed to sign up', detail: errorMessage });
+        });
     }
+  }
+
+  public get validateForm(): boolean {
+    return (
+      this.form.valid &&
+      this.form.controls['password'].value === this.form.controls['confirmPassword'].value &&
+      this.form.controls['acceptTerm'].value
+    );
   }
 }
