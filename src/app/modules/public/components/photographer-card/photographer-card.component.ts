fd31babdf7f4d795f8aa6962c8b34eed44087aa8
Amol Sontakke <amols@proximabiz.com>
2024-08-01 13:53:16 +0530

    Added card
    Signed-off-by: Amol Sontakke <amols@proximabiz.com>

--- src/app/modules/public/components/photographer-card/photographer-card.component.ts
+++ src/app/modules/public/components/photographer-card/photographer-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @Component({
   selector: 'app-photographer-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './photographer-card.component.html',
-  styleUrl: './photographer-card.component.scss'
+  styleUrl: './photographer-card.component.scss',
 })
 export class PhotographerCardComponent {
+  @Input() photographers: any[] = [];
 
+  public onViewProfile(): void {}
 }
--- src/app/modules/public/home/home.component.ts
+++ src/app/modules/public/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
-import { FooterComponent } from '../components/footer/footer.component';
 import { GoogleMapComponent } from '../components/google-map/google-map.component';
 import { PhotographerCardComponent } from '../components/photographer-card/photographer-card.component';
+import { FooterComponent } from '../../layout/components/footer/footer.component';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, GoogleMapComponent, PhotographerCardComponent],
+  imports: [HeaderComponent, FloatLabelModule, DropdownModule, FooterComponent, GoogleMapComponent, PhotographerCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public photographers: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  public states: any[] = [];
+}
